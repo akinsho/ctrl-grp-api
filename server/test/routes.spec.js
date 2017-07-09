8e04af8f0513ac285c565b6b11aa832cb9dec7d1
Akin909 <akin.sowemimo@gmail.com>
2017-07-09 19:54:39 +0100

    Add test for initial route getUsers

--- server/test/routes.spec.js
+++ server/test/routes.spec.js
@@ -4,3 +4,19 @@ const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('./../server.js');
+
+chai.use(chaiHttp);
+
+describe('API Routes', () => {
+  describe('GET /api/v1/users', () => {
+    it('should return all users', done => {
+      chai.request(server).get('/api/v1/users').end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.equal(1);
+        done();
+      });
+    });
+  });
+});
